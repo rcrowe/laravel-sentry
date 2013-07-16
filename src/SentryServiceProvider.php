cf44ef73eb0835f6c487fa056a3f063b0f1f5591
Rob Crowe <hello@vivalacrowe.com>
2013-07-16 10:37:20 +0100

    Fleshed out

--- src/SentryServiceProvider.php
+++ src/SentryServiceProvider.php
@@ -3,20 +3,41 @@
 namespace rcrowe\Sentry;
 
 use Illuminate\Support\ServiceProvider;
+use Raven_Client;
 
 class SentryServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        // TODO: Register config file
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @throws InvalidArgumentException Thrown with an invalid Sentry DSN.
+     */
     public function boot()
     {
-        $sentry = new Log($this->app['log']->getMonolog(), $this->app['events']);
+        // TODO: Grab these values from a config file
+        $enabled = true;
+        $dsn     = '';
+        $level   = 'error';
 
-        $dsn   = '';
-        $level = 'error';
+        if ($enabled) {
+            // Check Sentry DSN is valid
+            // Throws an InvalidArgumentException if it isn't
+            Raven_Client::parseDSN($dsn);
 
-        $sentry->setHandler($dsn, $level);
+            // Add the Raven handler to Monolog
+            // Log::error(...) will then send a message straight to Sentry
+            $sentry = new Log($this->app['log']->getMonolog());
+            $sentry->setRaven(new Raven_Client($dsn));
+            $sentry->addHandler($level);
+
+            // Store Sentry in the IoC
+            // Useful for getting at the Raven client to send custom messages
+            $this->app->instance('sentry', $sentry);
+        }
     }
 }

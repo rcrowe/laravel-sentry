f4bca632031d5863b23d3351df0cc3d86e29ca99
Rob Crowe <hello@vivalacrowe.com>
2013-07-16 08:49:09 +0100

    Proof of concept

--- src/SentryServiceProvider.php
+++ src/SentryServiceProvider.php
@@ -1,10 +1,22 @@
 <?php
 
-namespace rcrowe;
+namespace rcrowe\Sentry;
 
 use Illuminate\Support\ServiceProvider;
 
 class SentryServiceProvider extends ServiceProvider
 {
+    public function register()
+    {
+    }
 
+    public function boot()
+    {
+        $sentry = new Log($this->app['log']->getMonolog(), $this->app['events']);
+
+        $dsn   = '';
+        $level = 'error';
+
+        $sentry->setHandler($dsn, $level);
+    }
 }

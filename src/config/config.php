92d9ad1b23fb2ace5356518f5336e141d352d750
Rob Crowe <hello@vivalacrowe.com>
2013-07-18 22:29:41 +0100

    Config comments

--- src/config/config.php
+++ src/config/config.php
@@ -2,9 +2,18 @@
 
 return array(
 
+    /**
+     * Sentry DSN.
+     */
     'dsn' => '',
 
+    /**
+     * Environments were logging to Sentry is allowed.
+     */
     'environments' => array('prod', 'production'),
 
+    /**
+     * Log level (inclusive) at which to log to Sentry.
+     */
     'level' => 'error',
 );
